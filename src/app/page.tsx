6069432c2c37b1243109a2158a0844c97497f566
Sattwik13 <sattwikmanna6359@gmail.com>
2025-04-10 02:20:26 +0530

    Solving NavBar issue

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]"
+    >
+      <h1 className="text-2xl text-center">Sattwik Aur Code</h1>
+    </main>
+  )
+}
+
