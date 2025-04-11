69f12746cb1c2b25d3f6112569b2add508e334c8
Sattwik13 <sattwikmanna6359@gmail.com>
2025-04-11 19:42:35 +0530

    Create Animates Tooltip & wavy Background on a new section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import FeaturedCourses from "./components/FeaturedCourses"
 import HeroSection from "./components/HeroSection"
+import Instructor from "./components/Instructor"
 import MusicSchoolTestimonialCards from "./components/TestimonialCards"
 import UpcomingWebinars from "./components/UpcomingWebinars"
 import WhyChooseUs from "./components/WhyChooseUs"
@@ -13,6 +14,7 @@ export default function Home() {
       <WhyChooseUs />
       <MusicSchoolTestimonialCards />
       <UpcomingWebinars />
+      <Instructor />
     </main>
   )
 }
