2fe358c2b364f1da997596453da01601b526d156
Sattwik13 <sattwikmanna6359@gmail.com>
2025-04-15 01:20:12 +0530

    color correction

--- src/app/components/navbar-menu.tsx
+++ src/app/components/navbar-menu.tsx
@@ -113,7 +113,7 @@ export const HoveredLink = ({ children, ...rest }: any) => {
   return (
     <Link
       {...rest}
-      className="text-neutral-700 dark:text-neutral-200 hover:text-black "
+      className="text-neutral-700 dark:text-neutral-300 hover:text-white "
     >
       {children}
     </Link>
